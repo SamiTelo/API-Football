cf73903a070cb18e90a85b3fcd32bef17a4ad1c9
Sami Telo <samitelo@gmail.com>
2026-03-29 23:52:27 +0000

    feature/clean-mailservice

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -25,15 +25,19 @@ export class MailService {
     this.from = from;
     this.apiKey = apiKey;
 
+    // Log pour production : juste l'info que le service est initialisé
     this.logger.log(`MailService initialized with sender=${this.from}`);
   }
 
   async sendMail(to: string, subject: string, html: string) {
     try {
-      const response = await axios.post(
+      await axios.post(
         'https://api.brevo.com/v3/smtp/email',
         {
-          sender: { email: this.from },
+          sender: {
+            email: this.from,
+            name: 'Football-club',
+          },
           to: [{ email: to }],
           subject,
           htmlContent: html,
@@ -48,14 +52,10 @@ export class MailService {
       );
 
       this.logger.log(`Email sent to ${to} with subject "${subject}"`);
-      this.logger.debug(`Brevo response: ${JSON.stringify(response.data)}`);
     } catch (error: unknown) {
-      // fallback safe
       const message =
         error instanceof Error ? error.message : JSON.stringify(error);
-
       this.logger.error('Error sending email via Brevo API', message);
-
       throw new InternalServerErrorException("Impossible d'envoyer l'email");
     }
   }
